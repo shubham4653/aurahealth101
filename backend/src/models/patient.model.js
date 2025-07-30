e4118254a624b833105ac9c9afb1f0ef3d8943f8
shubham4653 <awarishubham4653@gmail.com>
2025-07-30 21:08:14 +0530

    Access tokeans and Refresh tokens and pasword hashing

--- backend/src/models/patient.model.js
+++ backend/src/models/patient.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Schema} from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const patientSchema = new Schema(
     {
@@ -36,5 +38,36 @@ const patientSchema = new Schema(
     {timestamps: true}
 )
 
+patientSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+    this.password = bcrypt.hashSync(this.password, 10)
+    next()
+})
+
+patientSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+    
+}
+
+patientSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id,
+            name: this.name,
+            email: this.email,  
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+patientSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id, 
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
 
 export const Patient = mongoose.model('Patient', patientSchema);
--- backend/src/models/provider.model.js
+++ backend/src/models/provider.model.js
@@ -1,4 +1,7 @@
 import mongoose,{Schema} from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const providerSchema = new Schema(
     {
@@ -20,5 +23,36 @@ const providerSchema = new Schema(
     { timestamps: true }
 )
 
+providerSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+    this.password = bcrypt.hashSync(this.password, 10)
+    next()
+})
+
+providerSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+    
+}
+
+providerSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id,
+            name: this.name,
+            email: this.email,  
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+providerSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id : this._id, 
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
 
 export const Provider = mongoose.model('Provider', providerSchema);
