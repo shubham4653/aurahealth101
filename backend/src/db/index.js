27ba6d69eb3e2e06909274e6aaae49591b2db5db
shubham4653 <awarishubham4653@gmail.com>
2025-07-30 12:53:04 +0530

    database connection sucessful

---
+++ backend/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+
+const connectDB = async() => {
+    try {
+        const connectionInscance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        console.log(`\nMongoDB connected !! DB HOST : ${connectionInscance.connection.host}`);
+    } catch (error) {
+        console.log('Error connecting to the database:', error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
